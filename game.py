d770e30d532882aa576cf2ffd9241ca5d5f82e11
Gwladys <gwladys.hardware@gmail.com>
2020-12-04 15:32:27 +0100

    Add the program

---
+++ game.py
@@ -0,0 +1,45 @@
+"""
+Guess the number in Python
+It's a terminal game
+The number to guess is between 1 and 100 and the player has 10 tries to guess it
+Beginner's Python program
+"""
+
+import random # for randint()
+
+print("\nGuess the number game\n")
+
+# make an infinite loop for the play again system
+while True:
+
+	# define the number to guess with randint()
+	number = random.randint(1, 100)
+	# initialize the counter
+	counter = 0
+
+	print("I'm thinking of a number between 1 and 100, you have 10 tries to guess it")
+
+	while counter < 10:
+		guess = int(input("Try to guess\n"))
+		counter+=1
+
+		if guess == number:
+			break
+		elif guess < number:
+			print("Too low!")
+		else:
+			print("Too high!")
+
+
+	if guess == number:
+		# the player won
+		print("Congratulations! You won in " + str(counter) + " tries.")
+	else:
+		# the player lost
+		print("You lost, I was thinking of " + str(number))
+
+	# ask to play again
+	if input("Do you wanna play again ? (y/n)\n") != "y":
+		break
+
+print("\nThanks for playing!\n")
